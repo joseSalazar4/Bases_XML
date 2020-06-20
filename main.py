375a33d0a5c0e1731903141d7b9b5d114116cf1d
Jose <mikomeka1@gmail.com>
2020-06-19 21:35:22 -0600

    Added method for Value changes

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
+from random import randint, random
 
 import lxml.etree as et
 
-#
-parser = et.XMLParser(remove_blank_text=True)
-tree = et.parse('Op_TESTING.xml', parser)
-root = tree.getroot()
+# Se deben generar las siguientes etiquetas:
+"""" 
+ <TransConsumo id="2" LecturaM3="" descripcion = ""> ESCRIBA 
+ id 1 es consumo normal
+ id 2 es ajuste debito (Reclamo de cliente)
+ id 3 ajuste credito (Error del men que hace la lectura)
+"""
 
-# Encontrar las empanadas
-# for operacionDia in root.findall('OperacionDia'):
-#    for propiedad in operacionDia.iter('Propiedad'):
-#         print(operacionDia.get('fecha'))
-#         print(propiedad.get('NumFinca'))
-#         print(propiedad.get('Valor'))
 
-child = et.Element("Consumo", NumeroFinca='12345', LecturaMedidorM3='1075')
-operacion = root.find('OperacionDia')
-operacion.insert(len(operacion), child)
+def modificarArchivo():
+    # Se parsea el xml viejo de operaciones
+    parser = et.XMLParser(remove_blank_text=True)
+    tree = et.parse('Op_TESTING.xml', parser)
+    root = tree.getroot()
 
-#Esto genera el archivo
-s = et.tostring(root, pretty_print=True)
-print(s.decode())
+    # Buscamos Fincas y su valor
+    matrizValoresFinca = []
+    listaFechas = []
 
-#Esribir
-tree.write('output.xml', pretty_print=True)
+    for operacionDia in root.findall('OperacionDia'):
+        fecha = operacionDia.get('fecha')
+        listaFechas.append(fecha)
+        for propiedad in operacionDia.iter('Propiedad'):
+            valor = propiedad.get('Valor')
+            propiedad = propiedad.get('NumFinca')
+            matrizValoresFinca.append([propiedad, valor])
+
+            fechaDeCambio = listaFechas[randint(0, len(listaFechas) - 1)]
+            """
+            Esto genera los cambios en las propiedades 
+            !Para Activar los triggers!
+            if randint(1, 100) > 95 and fechaDeCambio != fecha:
+                numeroFinca = randint(1, len(matrizValoresFinca) - 1)
+                fincaElegida = matrizValoresFinca.pop(numeroFinca)
+                valorActual = int(float(fincaElegida[1]))
+                nuevoValor = valorActual + (randint(int(valorActual / 8), int(valorActual / 4))
+                                            * ((-1) ** randint(1, 2)))
+                cambioFinca = et.Element("CambioPropiedad", NumFinca=fincaElegida[0], NuevoValor=str(nuevoValor))
+                operacionDia.insert(len(operacionDia), cambioFinca)
+            """
+            # if para los recibos
+
+    # Esto genera el archivo
+    s = et.tostring(root, pretty_print=True)
+    print(s.decode())
+
+    # Esribir
+    tree.write('output.xml', pretty_print=True)
+
+
+modificarArchivo()
